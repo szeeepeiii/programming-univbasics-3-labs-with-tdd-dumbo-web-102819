ea7f32fab2df5c7fdc029c5e6da0090dca2adfb5
Szepei <cheung.szepei@gmail.com>
2019-09-28 17:43:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@ second_number=5
 sum=first_number+second_number
 difference=first_number-second_number
 division=first_number/second_number
-multiply=first_number*second_number
+product=first_number*second_number
